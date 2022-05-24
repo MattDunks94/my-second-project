027623055b27f2e38b9a522de3acf5ae2a92d712
Matt <mjd014@live.co.uk>
2022-05-24 10:55:03 +0000

    added basic stying and added javascript function structure

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,19 @@
-function startGame() {
+// Getting start button and quiz area elements.
+let startButton = document.getElementById('start-btn');
+let quizArea = document.getElementById('quiz-area');
 
-    
+// Adding event listener to start button to execute startGame function.
+startButton.addEventListener('click', startGame);
+
+/**
+ * Adding class 'hide' to start button, so when 
+ * start button is clicked it will disappear. 
+ * Removing class 'hide' from quizArea element, so when 
+ * start button is clicked quizArea element appears.
+ */ 
+function startGame() {
+    startButton.classList.add('hide');
+    quizArea.classList.remove('hide');
 }
 
 function selectAnswer() {
