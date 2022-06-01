10a52b07518884cec09797588c99afe2cfa580b9
Matt <mjd014@live.co.uk>
2022-06-01 20:48:13 +0000

    added comments for created functions in .js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,29 @@
-/**
+//Function that holds all necessary functions for list app.
+function startList() {
+    /**
  * Getting the 'add item' button and adding event listener.
  * Added function that creates new element (list item) 
  * when user clicks 'add item' button.
  */
-let addButton = document.getElementById('addItemBtn');
-addButton.addEventListener("click", function addItem() {
+    let addButton = document.getElementById('addItemBtn');
+    addButton.addEventListener("click", function addItem() {
 
-    let newItem = document.createElement('div');
-    newItem.innerHTML = document.getElementById('box').value;
-    document.getElementById('list-container').appendChild(newItem);
-    newItem.className = "new-item";
+        let newItem = document.createElement('div');
+        newItem.innerHTML = document.getElementById('box').value;
+        document.getElementById('list-container').appendChild(newItem);
+        newItem.className = "new-item";
 
-    /**
-     * Adding remove button when user adds item.
-     * add event listener to newly created button
-     * which, when clicked, removes new item.
-     */
-    let removeBtn = document.createElement('button');
-    newItem.appendChild(removeBtn);
-    removeBtn.className = 'remove-btn';
-    removeBtn.innerHTML = 'X';
-    removeBtn.addEventListener('click', function removeItem() {
-        document.getElementById('list-container').removeChild(newItem);
+        /**
+         * Adding remove button when user adds item.
+         * add event listener to newly created button
+         * which, when clicked, removes new item.
+         */
+        let removeBtn = document.createElement('button');
+        newItem.appendChild(removeBtn);
+        removeBtn.className = 'remove-btn';
+        removeBtn.innerHTML = 'X';
+        removeBtn.addEventListener('click', function removeItem() {
+            document.getElementById('list-container').removeChild(newItem);
+        });
     });
-});
-
-
-
-
+};
