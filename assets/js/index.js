0ba157a12662494fa131b42129a2fe9b8135fa4a
Matt <mjd014@live.co.uk>
2022-06-01 20:44:20 +0000

    made function that creates a remove button when new item is added in .js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,30 @@
-//Getting the 'add item' button and adding event listener function.
+/**
+ * Getting the 'add item' button and adding event listener.
+ * Added function that creates new element (list item) 
+ * when user clicks 'add item' button.
+ */
 let addButton = document.getElementById('addItemBtn');
 addButton.addEventListener("click", function addItem() {
+
     let newItem = document.createElement('div');
     newItem.innerHTML = document.getElementById('box').value;
     document.getElementById('list-container').appendChild(newItem);
-    newItem.className = "new-item"
+    newItem.className = "new-item";
+
+    /**
+     * Adding remove button when user adds item.
+     * add event listener to newly created button
+     * which, when clicked, removes new item.
+     */
+    let removeBtn = document.createElement('button');
+    newItem.appendChild(removeBtn);
+    removeBtn.className = 'remove-btn';
+    removeBtn.innerHTML = 'X';
+    removeBtn.addEventListener('click', function removeItem() {
+        document.getElementById('list-container').removeChild(newItem);
+    });
 });
+
 
 
 
